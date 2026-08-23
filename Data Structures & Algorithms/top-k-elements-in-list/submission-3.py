17be2a0928f0542077c02fdef09a15cb86b4e3cc
Nathaniel Asiak <asiaknathan@gmail.com>
2026-08-23 23:41:31 +0000

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        Brute force solution 
+        ------------------
+        1. find the dictionary of nums, and frequency 
+        2. create an array and sort the values
+        3. get the values until the len k is reached
+        """
+        max_dict = {}
+
+        for num in nums:
+            max_dict[num] = 1 + max_dict.get(num, 0)
+        
+        top_keys = heapq.nlargest(k, max_dict, key=max_dict.get)
+        # arr = []
+        # for num, count in max_dict.items():
+        #     arr.append([count, num])
+        
+        # arr.sort()
+        
+        # res =  []
+        # while len(res) < k:
+        #     res.append(arr.pop()[1])
+        
+        return top_keys
